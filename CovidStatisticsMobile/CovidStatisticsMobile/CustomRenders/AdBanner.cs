5240f66f8759df610175eba1e62306b6eff93344
cameronpeters87 <cameronpeters87@gmail.com>
2020-09-21 12:35:22 +0200

    Put ads

---
+++ CovidStatisticsMobile/CovidStatisticsMobile/CustomRenders/AdBanner.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace CovidStatisticsMobile.CustomRenders
+{
+    public class AdBanner : View
+    {
+        public enum Sizes { Standardbanner, LargeBanner, MediumRectangle, FullBanner, Leaderboard, SmartBannerPortrait }
+        public Sizes Size { get; set; }
+        public AdBanner()
+        {
+            this.BackgroundColor = Color.Transparent;
+        }
+    }
+}
